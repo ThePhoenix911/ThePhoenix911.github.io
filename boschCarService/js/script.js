f3c1e3ea893a4099b01bb66d4c358cfccfc98cb0
ThePhoenix911 <93732213+ThePhoenix911@users.noreply.github.com>
2022-11-20 09:48:09 +0200

    Bosch Car Service file update 2

--- boschCarService/js/script.js
+++ boschCarService/js/script.js
@@ -774,111 +774,12 @@ function store(a, b, c, submitbtn, fieldList, compareDetails){
 
 
 
-// Customer reviews slideshow
-function custSlideshow() {
-    // Customer reviews slideshow
-    const menuIcon = document.querySelector(".hamburger-icon");
-    const navMenu = document.querySelector(".nav-menu");
-
-    menuIcon.addEventListener("click", ()=> {
-        menuIcon.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    });
-
-    document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", ()=> {
-        menuIcon.classList.remove("active");
-        navMenu.classList.remove("active");
-    }));
-
-    // Test
-
-    var slideIndex = 1;
-    showDivs(slideIndex);
-
-    function plusDivs(n) {
-    showDivs(slideIndex += n);
-    }
-
-    function currentDiv(n) {
-    showDivs(slideIndex = n);
-    }
-
-    function showDivs(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1};
-    if (n < 1) {slideIndex = slides.length};
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" fill", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " fill";
-    }
-}
 
 
 
 
-// Picture Slideshow
-function imgSlideshow() {
-    let slideNumber = 0;
 
-    $( ()=> {
-    
-          $("#leftArrPersonnel").click(e => {
-                if(slideNumber > 0) {
-    
-                      $("img.slideImages").animate(
-                            {left: "+=401px"}, 1000
-                      );
-    
-                      slideNumber--;
-    
-                      let currentSlide = $("img.slideImages")[slideNumber];
-    
-                      let slideCaption = $(currentSlide).attr("alt");
-    
-                      changeCaption(slideCaption);
-    
-                }
-          });
-    
-          $("#rightArrPersonnel").click(e => {
-                if(slideNumber < 11) {
-    
-                      $("img.slideImages").animate(
-                            {left: "-=401px"}, 1000
-                      );
-    
-                      slideNumber++;
-    
-                      let currentSlide = $("img.slideImages")[slideNumber];
-    
-                      let slideCaption = $(currentSlide).attr("alt");
-    
-                      changeCaption(slideCaption);
-    
-                }
-          });
-    
-          function changeCaption(captionText) {
-                $("div#caption").effect("blind", {
-                      mode: "hide",
-                      direction: "left"
-                }, 500, ()=> $("div#caption").text(captionText))
-                
-                .effect("blind", {
-                      mode: "show",
-                      direction: "left"
-                }, 500);
-                
-          }
-    });
-}
+
 
 
 /*
